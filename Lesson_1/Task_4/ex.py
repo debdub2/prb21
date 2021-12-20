5cd6444c9efa60d495fe0bb2a2ecc228343db182
debdub2 <95753626+debdub2@users.noreply.github.com>
2021-12-20 23:12:26 +0300

    Update ex.py

--- Lesson_1/Task_4/ex.py
+++ Lesson_1/Task_4/ex.py
@@ -1,19 +1,16 @@
-def possibility(y):       #Данная функция ввсчитывает возможность попасть в ячейку y при заданных условиях
-    x = a[0]
-    while x < t:
-        x += a[x-1]
-    if x == t:
+def possibility(y, listn):
+    x = listn[0]
+    while x < y:
+        x += listn[x-1]
+    if x == y:
         return(True)
     else:
         return(False)
 
 if __name__ == '__main__':
-    n=int(input()) #А ещё я не смог разобраться, как организовать в Python ввод в одну строку
-    t=int(input())
-    a=[0]*(n-1) #Создаём массив с числами ai
-    for i in range(n-1):
-        a[i]=int(input()) #Заполняем его данными необходимыми числами
-    if possibility(t) == True:  #Вызываем функцию и высчитываем возможность оказаться в ячейке t
+    n,t=map(int, input().split())
+    a = map(int, input().split(" "))
+    if possibility(t, list(a)) == True:
         print('Yes')
     else:
         print('No')
