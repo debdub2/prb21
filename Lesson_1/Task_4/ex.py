091e7af8a8296770f0781332adc10cde74a9c6e4
debdub2 <95753626+debdub2@users.noreply.github.com>
2021-12-20 19:04:03 +0300

    Create ex.py

---
+++ Lesson_1/Task_4/ex.py
@@ -0,0 +1,19 @@
+def possibility(y):       #Данная функция ввсчитывает возможность попасть в ячейку y при заданных условиях
+    x = a[0]
+    while x < t:
+        x += a[x-1]
+    if x == t:
+        return(True)
+    else:
+        return(False)
+
+if __name__ == '__main__':
+    n=int(input())
+    t=int(input())
+    a=[0]*(n-1) #Создаём массив с числами ai
+    for i in range(n-1):
+        a[i]=int(input()) #Заполняем его данными необходимыми числами
+    if possibility(t) == True:  #Вызываем функцию и высчитываем возможность оказаться в ячейке t
+        print('Yes')
+    else:
+        print('No')
