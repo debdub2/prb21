d858cfa22aae73f2661e4c6277bd003d9dc280a9
debdub2 <95753626+debdub2@users.noreply.github.com>
2021-12-21 00:39:24 +0300

    Update ex.py

--- Lesson_1/Task_5/ex.py
+++ Lesson_1/Task_5/ex.py
@@ -1,16 +1,17 @@
-def replacestrg(strg):
+def rplcstrg(strg, l):
     x = 0
-    while x < n-2:
-        if strg[x] == 'B' and s[x+1] =='G':
-            strg=strg.replace(s[x], 'B', 1)
-            strg=strg.replace(s[x+1], 'G', 1)
-            x +=1
-        x += 1
+    while x < l-1:
+        if strg[x] == "B" and strg[x+1] == "G": 
+            strg = strg[:x] + "G" + strg[x+1:]
+            strg = strg[:x+1] + "B" + strg[x+2:]
+            x += 2
+        else:
+            x += 2
     return(strg)
-    
+
 if __name__ == '__main__':
     n, t = map(int, input().split())
-    s=input()
+    s = input()
     for i in range(t):
-        s=s.replace(s, replacestrg(s))
+        s = rplcstrg(s, n)
     print(s)
