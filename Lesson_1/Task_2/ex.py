72d3439b2312cb33577a8ec04378209ada018f59
debdub2 <95753626+debdub2@users.noreply.github.com>
2021-12-20 14:35:11 +0300

    Update ex.py

--- Lesson_1/Task_2/ex.py
+++ Lesson_1/Task_2/ex.py
@@ -1,9 +1,9 @@
 def main(strg):
-    return(strg == strg[::-1])
+    if strg == strg[::-1]:
+       print('This is palindrome.')
+    else:
+      print('This is not palindrome.')
 
 if __name__ == '__main__':
     input_string=input()
-    if main(input_string) == 1:
-      print('This is palindrome.')
-    else:
-      print('This is not palindrome.')
+    main(input_string)
